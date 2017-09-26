3c893991d6697a900a849a70f3627988dba4c79b
Mullykitten <32171347+Mullykitten@users.noreply.github.com>
2017-09-26 11:02:26 -0400

    Fixed program

--- studentInfo.py
+++ studentInfo.py
@@ -1,30 +1,38 @@
 def main():
   students = [
-    Student("Larsson", 37),
-    Student("BonJovi", 55),
-  ]
-
+    Student("Halsted","Larsson", 37),
+    Student("John","BonJovi", 55),
+    Student("Kathryn","Coombs", 16),
+    Student("Tucker","Brooks", 16),
+    Student("Al","Paccino", 78),
+    Student("Heather", "Duke", 17),
+    Student("Ben","Kiesel", 37),
+    Student("Clark","Kent", 42),
+    Student("Elijah","Booker", 18),
+    Student("Star","Lord", 37),
+]
   printHeader()
   selection = getUserSelection()
   if selection == 0:
     printStudentsByAge(students)
   elif selection == 1:
-    pass
+    printStudentsByLName(students)
   elif selection == 2:
-    pass
+    printStudentsByFName(students)
   else:
     print "SELECTION NOT RECOGNIZED"
 
 
 class Student:
-  def __init__(self, lastName, age):
+  def __init__(self,firstName, lastName, age):
     self.lastName = lastName
     self.age = age
-    self.firstName = "JOHN"
+    self.firstName = firstName
 
   def assignRandomName(self):
     pass
 
+Import random:
   def assignRandomAge(self):
     self.age = random.randint(0,100)
 
@@ -34,7 +42,7 @@ class Student:
   def assignRandomHeight(self, isMetric):
     pass
 
-inputQuestions = [ 
+inputQuestions = [
   "For STUDENTS BY AGE, type 0",
   "For STUDENTS BY LAST NAME, type 1",
   "For STUDENTS BY FIRST NAME, type 3",
@@ -46,22 +54,30 @@ def getUserSelection():
   print (inputQuestions[0])
   print (inputQuestions[1])
   print (inputQuestions[2])
+  print (inputQuestions[3])
+  print (inputQuestions[4])
   return input("Type selection and press enter:")
 
 def printHeader():
-    print("HEADER TEXT HERE")
+    print("STUDENT INFO")
 
 def printStudentsByAge(students):
   print ("----Students By Age-----")
   sortStudents = sorted(students, key=lambda student: student.age)
-  for student in students:
+  for student in sortStudents:
     print student.lastName + ", " + student.firstName + ", " + str(student.age)
 
 def printStudentsByLName(students):
   print ("----Students By -----")
+  sortStudents = sorted(students, key=lambda student: student.lastName)
+  for student in sortStudents:
+    print student.lastName + ", " + student.firstName + ", " + str(student.age)
 
 def printStudentsByFName(students):
   print ("----Students By -----")
+  sortStudents = sorted(students, key=lambda student: student.firstName)
+  for student in sortStudents:
+    print student.lastName + ", " + student.firstName + ", " + str(student.age)
 
 def printSumAge(students):
   print ("Answer:")
